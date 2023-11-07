b6054845882bf4534cf8405941a4864068a618e0
satriabrianydn <satriabrianyudanto16@gmail.com>
2023-11-07 14:08:28 +0700

    menghapus panah carousel

--- index.php
+++ index.php
@@ -102,13 +102,13 @@
         </div>
       </div> -->
 
-      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+      <!-- <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
         <span class="carousel-control-prev-icon bx bx-chevron-left" aria-hidden="true"></span>
       </a>
 
       <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
         <span class="carousel-control-next-icon bx bx-chevron-right" aria-hidden="true"></span>
-      </a>
+      </a> -->
 
     </div>
   </section><!-- End Hero -->
