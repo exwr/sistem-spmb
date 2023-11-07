725ed719cdee15db847625f2abaf509101c3bd9f
exwr <ekah4613@gmail.com>
2023-11-07 13:37:32 +0700

    tambah file

---
+++ index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SPMB 2023 | Universitas ALETA</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <style>
+    h3, h4, h5 {
+      color: #fff;
+    }
+  </style>
+
+  
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.php"><span>SPMB 2023</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href=gi"index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active " href="index.php">Home</a></li>
+          <li><a href="daftar-prodi.php">Daftar Prodi</a></li>
+          <li><a href="biaya-pendaftaran.php">Biaya Pendaftaran</a></li>
+          <li class="dropdown"><a href="#"><span>Informasi Fakultas</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Fakultas Ilmu Komputer</a></li>
+              <li><a href="#">Fakultas Ilmu Kesehatan</a></li>
+              <li><a href="#">Fakultas Sains & Teknologi</a></li>
+              <li><a href="#">Fakultas Hukum & Bisnis</a></li>
+
+            </ul>
+          </li>
+          <li><a href="Register.php">Register</a></li>
+          <li><a href="login.php">Login</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex justify-cntent-center align-items-center">
+    <div id="heroCarousel" class="container carousel carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+      <!-- Slide 1 -->
+      <div class="carousel-item active">
+        <div class="carousel-container">
+          <h3 class="animate__animated animate__fadeInDown"><span>Sistem Pendaftaran Mahasiswa Baru</span></h3>
+          <h3 class="animate__animated animate__fadeInDown"><span>Universitas ALETA</span></h3>
+          <p class="animate__animated animate__fadeInUp">Selamat datang di Sistem Pendaftaran Mahasiswa Baru Universitas ALETA. Sistem ini dirancang untuk memberikan kenyamanan, transparansi, dan efisiensi dalam proses penerimaan mahasiswa baru. Kami sangat bangga dapat menjadi bagian dari perjalanan pendidikan Anda dan membantu Anda mencapai impian akademis Anda.</p>
+        </div>
+      </div>
+
+      <!-- Slide 2 -->
+      <!-- <div class="carousel-item">
+        <div class="carousel-container">
+          <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
+          <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+          <a href="" class="btn-get-started animate__animated animate__fadeInUp">Read More</a>
+        </div>
+      </div> -->
+
+      <!-- Slide 3 -->
+      <!-- <div class="carousel-item">
+        <div class="carousel-container">
+          <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
+          <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+          <a href="" class="btn-get-started animate__animated animate__fadeInUp">Read More</a>
+        </div>
+      </div> -->
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bx bx-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bx bx-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Services Section ======= -->
+    <section class="services">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="icon-box icon-box-pink">
+              <div class="icon"><i class="bx bx-laptop"></i></div>
+              <h4 class="title"><a href="">Fakultas Ilmu komputer</a></h4>
+              <p class="description">Bidang studi yang berfokus pada pengembangan, perancangan, pemeliharaan, dan pengelolaan perangkat keras, perangkat lunak, serta infrastruktur teknologi informasi</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box icon-box-cyan">
+              <div class="icon"><i class="bx bx-health"></i></div>
+              <h4 class="title"><a href="">Fakultas Ilmu Kesehatan</a></h4>
+              <p class="description">Lembaga pendidikan tinggi yang fokus pada pendidikan, penelitian, dan pengembangan dalam bidang kesehatan dan ilmu kedokteran.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box icon-box-green">
+              <div class="icon"><i class="bx bx-atom"></i></div>
+              <h4 class="title"><a href="">Fakultas Sains & Teknologi</a></h4>
+              <p class="description">Lembaga pendidikan tinggi yang memiliki fokus utama pada ilmu pengetahuan alam, teknologi, dan teknik.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box icon-box-blue">
+              <div class="icon"><i class="bx bx-book"></i></div>
+              <h4 class="title"><a href="">Fakultas Hukum & Bisnis</a></h4>
+              <p class="description">Sebuah lembaga pendidikan tinggi yang menyediakan pendidikan dalam dua disiplin utama: hukum dan bisnis.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section class="why-us section-bg" data-aos="fade-up" date-aos-delay="200">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6 video-box">
+            <img src="assets/img/why-us.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-6 d-flex flex-column justify-content-center p-5">
+
+            <div>
+              <h4 class="title"><a href="">Tentang Kampus</a></h4>
+              <p class="description">Universitas adalah lembaga pendidikan tinggi yang menyediakan berbagai program studi dan tingkat pendidikan, mulai dari sarjana hingga doktoral. Mereka biasanya fokus pada penelitian, pengembangan ilmu pengetahuan, dan pendidikan tinggi. </p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+    <!-- ======= Features Section ======= -->
+    <section class="features">
+      <div class="container">
+
+        <div class="section-title">
+          <h1>Visi & Misi</h1>
+          <p>Berikut adalah Visi & Misi dari Universitas Konoha</p>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-md-5">
+            <img src="assets/img/features-1.svg" class="img-fluid" alt="">
+          </div>
+          <div class="col-md-7 pt-4">
+            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+            <p class="fst-italic">
+              <h1>Visi</h1>
+            </p>
+            <ul>
+              <li><i class=""></i>"Menjadi lembaga pendidikan unggulan yang diakui secara global, mendorong inovasi, dan mempersiapkan mahasiswa untuk menjadi pemimpin yang tangguh dalam berbagai disiplin ilmu".</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-md-5 order-1 order-md-2">
+            <img src="assets/img/features-2.svg" class="img-fluid" alt="">
+          </div>
+          <div class="col-md-7 pt-5 order-2 order-md-1">
+            <h3>Corporis temporibus maiores provident</h3>
+            <p class="fst-italic">
+             <h1> Misi </h1>
+            </p>
+            <p>1.Menyediakan program pendidikan tinggi yang berkualitas untuk mahasiswa dari berbagai latar belakang, mengutamakan pembelajaran, penelitian, dan pengembangan pribadi.</p>
+            <p>2.Menjadi pusat penelitian dan inovasi yang aktif, mendukung penemuan baru, pengembangan teknologi, dan kontribusi terhadap pengetahuan global.</p>
+            <p>3.Menciptakan lingkungan yang inklusif, ramah, dan beragam di mana semua individu dihargai dan didorong untuk berkontribusi.</p>
+            <p>4.Menyediakan manajemen dan pengelolaan universitas yang efisien dan transparan untuk memastikan pencapaian visi dan misi universitas.</p>
+            <p>5.Meningkatkan kualitas hidup mahasiswa dengan menyediakan fasilitas, layanan, dan lingkungan yang mendukung pertumbuhan dan kesejahteraan mereka.</p>
+            <p>6.Memegang teguh prinsip-prinsip etika dan integritas dalam semua aspek kegiatan universitas.</p>
+            
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Features Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Daftar Prodi</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Biaya Pendaftaran</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi Fakultas </a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Login</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              jln.Slamet Riyadi, no 18 <br>
+              Surakarta, Kode Pos 57112<br>
+              Jawa Tengah <br><br>
+              <strong>Phone:</strong> +621234567890<br>
+              <strong>Email:</strong> Universitas ALETA@ac.id<br>
+            </p>
+
+          </div>
+
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Universitas ALETA</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/ -->
+        Designed by <a href="https://bootstrapmade.com/">Universitas ALETA</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
