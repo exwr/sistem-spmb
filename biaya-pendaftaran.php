a1e21509006009b1e820c7b3a0b8a6f7c1cc8d33
satriabrianydn <satriabrianyudanto16@gmail.com>
2023-11-09 17:04:16 +0700

    ganti copyright footer

--- biaya-pendaftaran.php
+++ biaya-pendaftaran.php
@@ -330,11 +330,7 @@
         &copy; Copyright <strong><span>Universitas ALETA</strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/ -->
-        Designed by <a href="https://bootstrapmade.com/">Universitas ALETA</a>
+        Dikelola Oleh Tim IT <a href="index.php">Universitas ALETA</a>
       </div>
     </div>
   </footer>
