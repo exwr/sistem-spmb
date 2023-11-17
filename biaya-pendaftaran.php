14d306cae98806bdaf6b94e5a66c19c988f9c364
satriabrianydn <satriabrianyudanto16@gmail.com>
2023-11-17 15:56:34 +0700

    fix footer biaya-pendaftaran

--- biaya-pendaftaran.php
+++ biaya-pendaftaran.php
@@ -1,220 +1,223 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>SPMB 2023 | Universitas ALETA</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+<title>SPMB 2023 | Universitas ALETA</title>
+<meta content="" name="description">
+<meta content="" name="keywords">
 
- <!-- Favicons -->
- <link href="assets/img/logo/logo.png" rel="icon">
+<!-- Favicons -->
+<link href="assets/img/logo/logo.png" rel="icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<!-- Vendor CSS Files -->
+<link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-  <style>
-    h3, h4, h5 {
-      color: #fff;
-    }
-  </style>
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center ">
-    <div class="container d-flex justify-content-between align-items-center">
+<!-- Template Main CSS File -->
+<link href="assets/css/style.css" rel="stylesheet">
+<style>
+  h3,
+  h4,
+  h5 {
+    color: #fff;
+  }
+</style>
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top d-flex align-items-center ">
+  <div class="container d-flex justify-content-between align-items-center">
 
     <div class="logo">
-        <a href="index.php">
-          <img src="assets/img/logo/logo.png" alt="" class="img-fluid">
-          <h1 class="text-light"><span>SPMB 2023</span></h1>
-        </a>
-      </div>
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="daftar-prodi.php">Daftar Prodi</a></li>
-          <li><a class="active" href="biaya-pendaftaran.php">Biaya Pendaftaran</a></li>
-          <li class="dropdown"><a href="#"><span>Informasi Fakultas</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Fakultas Ilmu Komputer</a></li>
-              <li><a href="#">Fakultas Ilmu Kesehatan</a></li>
-              <li><a href="#">Fakultas Sains & Teknologi</a></li>
-              <li><a href="#">Fakultas Hukum & Bisnis</a></li>
-            </ul>
-          </li>
-          <li><a href="register.php">Register</a></li>
-          <li><a href="login.php">Login</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
+      <a href="index.php">
+        <img src="assets/img/logo/logo.png" alt="" class="img-fluid">
+        <h1 class="text-light"><span>SPMB 2023</span></h1>
+      </a>
     </div>
-  </header>
-  <!-- End Header -->
 
-    <!-- ======= Our Services Section ======= -->
-    <section class="breadcrumbs">
-      <div class="container">
+    <nav id="navbar" class="navbar">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="daftar-prodi.php">Daftar Prodi</a></li>
+        <li><a class="active" href="biaya-pendaftaran.php">Biaya Pendaftaran</a></li>
+        <li class="dropdown"><a href="#"><span>Informasi Fakultas</span> <i class="bi bi-chevron-down"></i></a>
+          <ul>
+            <li><a href="#">Fakultas Ilmu Komputer</a></li>
+            <li><a href="#">Fakultas Ilmu Kesehatan</a></li>
+            <li><a href="#">Fakultas Sains & Teknologi</a></li>
+            <li><a href="#">Fakultas Hukum & Bisnis</a></li>
+          </ul>
+        </li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav><!-- .navbar -->
 
-        <div class="d-flex justify-content-between align-items-center">
-          <h2>Biaya Pendaftaran</h2>
-          <ol>
-            <li><a href="index.php">Home</a></li>
-            <li>Biaya Pendaftaran</li>
-          </ol>
+  </div>
+</header>
+<!-- End Header -->
+
+<!-- ======= Our Services Section ======= -->
+<section class="breadcrumbs">
+  <div class="container">
+
+    <div class="d-flex justify-content-between align-items-center">
+      <h2>Biaya Pendaftaran</h2>
+      <ol>
+        <li><a href="index.php">Home</a></li>
+        <li>Biaya Pendaftaran</li>
+      </ol>
+    </div>
+</section>
+<!-- ======= Service Details Section ======= -->
+<section class="service-details">
+  <div class="container">
+
+    <div class="row">
+      <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <div class="card">
+          <div class="card-img">
+            <img src="assets/img/service-details-1.jpg" alt="...">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title"><a href="#">Fakultas Ilmu Komputer</a></h5>
+            <p class="card-text">S1 Sistem Informasi = Rp 10.000.000</p>
+            <p class="card-text">S1 Teknik Informatika = Rp 13.000.000</p>
+            <p class="card-text">D4 Teknologi Rekayasa Perangkat Lunak = Rp 10.000.000</p>
+            <p class="card-text">D3 Teknik Komputer = Rp 12.000.000</p>
+          </div>
         </div>
-    <!-- ======= Service Details Section ======= -->
-    <section class="service-details">
-      <div class="container">
-
-        <div class="row">
-          <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-            <div class="card">
-              <div class="card-img">
-                <img src="assets/img/service-details-1.jpg" alt="...">
-              </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="#">Fakultas Ilmu Komputer</a></h5>
-                <p class="card-text">S1 Sistem Informasi = Rp 10.000.000</p>
-                <p class="card-text">S1 Teknik Informatika = Rp 13.000.000</p>
-                <p class="card-text">D4 Teknologi Rekayasa Perangkat Lunak = Rp 10.000.000</p>
-                <p class="card-text">D3 Teknik Komputer = Rp 12.000.000</p>
-              </div>
-            </div>
+      </div>
+      <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <div class="card">
+          <div class="card-img">
+            <img src="assets/img/service-details-2.jpg" alt="...">
           </div>
-          <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-            <div class="card">
-              <div class="card-img">
-                <img src="assets/img/service-details-2.jpg" alt="...">
-              </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="#">Fakultas Ilmu Kesehatan</a></h5>
-                <p class="card-text">S1 Farmasi = Rp. 15.000.000</p>
-                <p class="card-text">S1 Keperawatan = Rp. 17.000.000</p>
-                <p class="card-text">S1 Administrasi Rumah Sakit = Rp. 14.000.000</p>
-                <p class="card-text">D3 Kebidanan = Rp. 10.000.000</p>
-                <p class="card-text">D3 Keperawatan = Rp. 12.000.000</p>
-              </div>
-            </div>
-
-          </div>
-          <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-            <div class="card">
-              <div class="card-img">
-                <img src="assets/img/service-details-3.jpg" alt="...">
-              </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="#">Fakultas Sains & Teknologi</a></h5>
-                <p class="card-text">S1 Teknik Industri = Rp. 11.000.000</p>
-                <p class="card-text">S1 Agribisnis = Rp. 12.000.000</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-            <div class="card">
-              <div class="card-img">
-                <img src="assets/img/service-details-4.jpg" alt="...">
-              </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="#">Fakultas Hukum & Bisnis</a></h5>
-                <p class="card-text">S1 Manajemen = Rp. 10.000.000</p>
-                <p class="card-text">S1 Hukum = Rp. 13.000.000</p>
-                <p class="card-text">S1 Akuntansi = Rp. 12.000.000</p>
-              </div>
-            </div>
+          <div class="card-body">
+            <h5 class="card-title"><a href="#">Fakultas Ilmu Kesehatan</a></h5>
+            <p class="card-text">S1 Farmasi = Rp. 15.000.000</p>
+            <p class="card-text">S1 Keperawatan = Rp. 17.000.000</p>
+            <p class="card-text">S1 Administrasi Rumah Sakit = Rp. 14.000.000</p>
+            <p class="card-text">D3 Kebidanan = Rp. 10.000.000</p>
+            <p class="card-text">D3 Keperawatan = Rp. 12.000.000</p>
           </div>
         </div>
 
       </div>
-    </section><!-- End Service Details Section -->
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Daftar Prodi</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Biaya Pendaftaran</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi Fakultas </a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Login</a></li>
-            </ul>
+      <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <div class="card">
+          <div class="card-img">
+            <img src="assets/img/service-details-3.jpg" alt="...">
           </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Website Fakultas</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Ilmu Komputer</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Ilmu Kesehatan</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Sains & Teknologi</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Hukum & Bisnis</a></li>
-            </ul>
+          <div class="card-body">
+            <h5 class="card-title"><a href="#">Fakultas Sains & Teknologi</a></h5>
+            <p class="card-text">S1 Teknik Industri = Rp. 11.000.000</p>
+            <p class="card-text">S1 Agribisnis = Rp. 12.000.000</p>
           </div>
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h4>Contact Us</h4>
-            <p>
-              Jl. Slamet Riyadi, No. 18 <br>
-              Surakarta, Kode Pos 57112<br>
-              Jawa Tengah <br><br>
-              <strong>Phone:</strong> +621234567890<br>
-              <strong>Email:</strong> Universitas ALETA@ac.id<br>
-            </p>
-
+        </div>
+      </div>
+      <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <div class="card">
+          <div class="card-img">
+            <img src="assets/img/service-details-4.jpg" alt="...">
           </div>
-
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-            </div>
+          <div class="card-body">
+            <h5 class="card-title"><a href="#">Fakultas Hukum & Bisnis</a></h5>
+            <p class="card-text">S1 Manajemen = Rp. 10.000.000</p>
+            <p class="card-text">S1 Hukum = Rp. 13.000.000</p>
+            <p class="card-text">S1 Akuntansi = Rp. 12.000.000</p>
           </div>
-
         </div>
       </div>
     </div>
 
+  </div>
+</section><!-- End Service Details Section -->
+
+<!-- ======= Footer ======= -->
+<footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+  <div class="footer-top">
     <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Universitas ALETA</strong>. All Rights Reserved
+      <div class="row">
+
+        <div class="col-lg-3 col-md-6 footer-links">
+          <h4>Links</h4>
+          <ul>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Daftar Prodi</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Biaya Pendaftaran</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi Fakultas </a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Login</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+          <h4>Website Fakultas</h4>
+          <ul>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Ilmu Komputer</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Ilmu Kesehatan</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Sains & Teknologi</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Hukum & Bisnis</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-contact">
+          <h4>Contact Us</h4>
+          <p>
+            Jl. Slamet Riyadi, No. 18 <br>
+            Surakarta, Kode Pos 57112<br>
+            Jawa Tengah <br><br>
+            <strong>Phone:</strong> +621234567890<br>
+            <strong>Email:</strong> Universitas ALETA@ac.id<br>
+          </p>
+
+        </div>
+
+        <div class="social-links mt-3">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
       </div>
-      <div class="credits">
-        Dikelola Oleh Tim IT <a href="index.php">Universitas ALETA</a>
-      </div>
+
     </div>
-  </footer>
+  </div>
+  </div>
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Universitas ALETA</strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      Dikelola Oleh Tim IT <a href="index.php">Universitas ALETA</a>
+    </div>
+  </div>
+</footer>
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+<!-- Vendor JS Files -->
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
 
 </body>
 
