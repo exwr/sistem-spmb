70146f537df3ad3c9d3118d4935bf71ab445d927
satriabrianydn <satriabrianyudanto16@gmail.com>
2023-11-20 21:56:20 +0700

    revisi footer

--- biaya-pendaftaran.php
+++ biaya-pendaftaran.php
@@ -137,64 +137,62 @@
 
 <!-- ======= Footer ======= -->
 <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
-  <div class="footer-top">
-    <div class="container">
-      <div class="row">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
 
-        <div class="col-lg-3 col-md-6 footer-links">
-          <h4>Links</h4>
-          <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Daftar Prodi</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Biaya Pendaftaran</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi Fakultas </a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Login</a></li>
-          </ul>
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Links</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Daftar Prodi</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Biaya Pendaftaran</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Informasi Fakultas </a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Login</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg+3 col-md-6 footer-links">
+                        <h4>Website Fakultas</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Ilmu Komputer</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Ilmu Kesehatan</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Sains & Teknologi</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Hukum & Bisnis</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h4>Contact Us</h4>
+                        <p>
+                            Jl. Slamet Riyadi, No. 18 <br>
+                            Surakarta, Kode Pos 57112<br>
+                            Jawa Tengah <br><br>
+                            <strong>Phone:</strong> +621234567890<br>
+                            <strong>Email:</strong> Universitas ALETA@ac.id<br>
+                        </p>
+                    </div>
+
+                    <div class="social-links mt-3">
+                        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div class="col-lg-3 col-md-6 footer-links">
-          <h4>Website Fakultas</h4>
-          <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Ilmu Komputer</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Ilmu Kesehatan</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Sains & Teknologi</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Fakultas Hukum & Bisnis</a></li>
-          </ul>
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Universitas ALETA</strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                Dikelola Oleh Tim IT <a href="index.php">Universitas ALETA</a>
+            </div>
         </div>
-
-        <div class="col-lg-3 col-md-6 footer-contact">
-          <h4>Contact Us</h4>
-          <p>
-            Jl. Slamet Riyadi, No. 18 <br>
-            Surakarta, Kode Pos 57112<br>
-            Jawa Tengah <br><br>
-            <strong>Phone:</strong> +621234567890<br>
-            <strong>Email:</strong> Universitas ALETA@ac.id<br>
-          </p>
-
-        </div>
-
-        <div class="social-links mt-3">
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-        </div>
-      </div>
-
-    </div>
-  </div>
-  </div>
-
-  <div class="container">
-    <div class="copyright">
-      &copy; Copyright <strong><span>Universitas ALETA</strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-      Dikelola Oleh Tim IT <a href="index.php">Universitas ALETA</a>
-    </div>
-  </div>
-</footer>
+    </footer>
+    <!-- End Footer -->
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
