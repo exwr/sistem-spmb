a77d575d4bbadcb8a65a9cb703a443ed3388b5ca
satriabrianydn <satriabrianyudanto16@gmail.com>
2023-11-22 21:57:15 +0700

    merapikan susunan code

--- login.php
+++ login.php
@@ -219,7 +219,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </main>
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
+    
+    <!-- Vendor JS Files -->
     <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
     <script src="assets/vendor/aos/aos.js"></script>
     <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -228,6 +229,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
     <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
     <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
     <script src="assets/js/main.js"></script>
     <script>
         function togglePasswordVisibility() {
