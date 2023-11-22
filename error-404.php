86fe0d97c6e3d8d720feedd847a9e06f8eb00f6e
satriabrianydn <satriabrianyudanto16@gmail.com>
2023-11-22 22:00:14 +0700

    perbaikan tata letak code

--- error-404.php
+++ error-404.php
@@ -6,6 +6,8 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title>Error 404 | Universitas ALETA</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
   <!-- Favicons -->
   <link href="assets/img/logo/logo.png" rel="icon">
